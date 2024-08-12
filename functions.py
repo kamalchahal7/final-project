ed7521337cd671c36dd6262e9b8e18fdaf656269
Kamal <kamalxchahal@gmail.com>
2024-08-12 19:17:24 -0400

    Rectified All Image Rendering Issues & Fixed Pokedex # Search

--- functions.py
+++ functions.py
@@ -4,15 +4,14 @@ from cs50 import SQL
 db = SQL("sqlite:///pokedex.db")
 
 def lookup(value):
-    pokemon = db.execute("SELECT * FROM pokemon WHERE name LIKE '%' || ? || '%' COLLATE NOCASE", (value,))
-    if not pokemon:
-        try: 
-            value = int(value)
-        except ValueError:
-            return False
-        pokemon = db.execute("SELECT * FROM pokemon WHERE pokedex_num = ?", (value,))
-        if not pokemon:
-            return False
+    if value is None:
+        return False
+    try: 
+        value = int(value)
+    except ValueError:
+        pokemon = db.execute("SELECT * FROM pokemon WHERE REPLACE(REPLACE(name, '(', ''), ')', '') LIKE '%' || REPLACE(REPLACE(?, '(', ''), ')', '') || '%' COLLATE NOCASE", (value,))
+        return pokemon
+    pokemon = db.execute("SELECT * FROM pokemon WHERE pokedex_num = ?", (value,))
     return pokemon
 
 def search():
